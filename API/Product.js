c6516371c027a3aa9702d35299b9faa8d4983248
Alma Breniz <coreanosj@gmail.com>
2024-08-04 18:25:41 -0600

    product

---
+++ API/Product.js
@@ -0,0 +1,32 @@
+const products = [
+    { product_id: 1, name: 'Galletas Marías', description: 'Abarrotes', price: 15.00, SKU: 'SKU001' },
+    { product_id: 2, name: 'Yakult', description: 'Lácteos', price: 7.00, SKU: 'SKU002' },
+    { product_id: 3, name: 'Aceite', description: 'Abarrotes', price: 39.99, SKU: 'SKU003' },
+    { product_id: 4, name: 'Arroz', description: 'Abarrotes', price: 19.99, SKU: 'SKU004' },
+    { product_id: 5, name: 'Frijol', description: 'Abarrotes', price: 29.99, SKU: 'SKU005' },
+    { product_id: 6, name: 'Leche', description: 'Lácteos', price: 19.99, SKU: 'SKU006' },
+    { product_id: 7, name: 'Azúcar', description: 'Abarrotes', price: 29.99, SKU: 'SKU007' },
+    { product_id: 8, name: 'Café', description: 'Abarrotes', price: 9.99, SKU: 'SKU008' },
+    { product_id: 9, name: 'Sal', description: 'Abarrotes', price: 19.99, SKU: 'SKU009' },
+    { product_id: 10, name: 'Mayonesa', description: 'Abarrotes', price: 29.99, SKU: 'SKU010' },
+    { product_id: 11, name: 'Harina', description: 'Harinas', price: 9.99, SKU: 'SKU011' },
+    { product_id: 12, name: 'Huevo', description: 'Abarrotes', price: 79.99, SKU: 'SKU012' },
+    { product_id: 13, name: 'Gelatina', description: 'Abarrotes', price: 19.99, SKU: 'SKU013' },
+    { product_id: 14, name: 'Sopa', description: 'Abarrotes', price: 15.99, SKU: 'SKU014' },
+    { product_id: 15, name: 'Vinagre', description: 'Abarrotes', price: 29.99, SKU: 'SKU015' },
+    { product_id: 16, name: 'Queso', description: 'Lácteos', price: 30.99, SKU: 'SKU016' },
+    { product_id: 17, name: 'Mantequilla', description: 'Lácteos', price: 49.99, SKU: 'SKU017' },
+    { product_id: 18, name: 'Media crema', description: 'Lácteos', price: 19.99, SKU: 'SKU018' },
+    { product_id: 19, name: 'Palomitas', description: 'Botanas', price: 15.00, SKU: 'SKU019' },
+    { product_id: 20, name: 'Papel higiénico', description: 'Higiene personal', price: 79.99, SKU: 'SKU020' },
+    { product_id: 21, name: 'Toallas sanitarias', description: 'Higiene personal', price: 60.99, SKU: 'SKU021' },
+    { product_id: 22, name: 'Pasta de dientes', description: 'Higiene personal', price: 59.99, SKU: 'SKU022' },
+    { product_id: 23, name: 'Jabón de tocador', description: 'Higiene personal', price: 49.99, SKU: 'SKU023' },
+    { product_id: 24, name: 'Cloro', description: 'Uso doméstico', price: 29.99, SKU: 'SKU024' },
+    { product_id: 25, name: 'Cerillos', description: 'Uso doméstico', price: 19.99, SKU: 'SKU025' },
+    { product_id: 26, name: 'Cigarros', description: 'Otros', price: 59.99, SKU: 'SKU026' }
+];
+
+// Mostrar los datos en la consola como una tabla
+console.table(products);
+
